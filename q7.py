d69342c590a575ab245f4d64b63d09b503dcbf7c
mohit-madgicaltechdom.com <mohit.jha@madgicaltechdom.com>
2023-08-13 16:36:28 +0530

    pushing python file

---
+++ q7.py
@@ -0,0 +1,7 @@
+# check a program if a strind is palidrome or not:
+a=input("enter the string:")
+b=a[-1::-1]
+if (a==b):
+    print('it is palindrome')
+else :
+     print("it is not palindrome")  
